914520f0688a8952c748e53d0693dfb81fa1dc70
Vamit06 <workloadamit@gmail.com>
2023-12-01 00:18:14 -0500

    Completed the remaining routes, put and delete

--- src/api/users.py
+++ src/api/users.py
@@ -22,6 +22,44 @@ class Users(Resource):
             api.abort(404, f"User {user_id} does not exist")
         return user, 200
     
+    #delete CRUD
+    #Delete user using id
+    @api.expect(user)
+    def delete(self,user_id):
+        user = User.query.filter_by(id=user_id).first()
+        email = user.email
+        response_object = {} 
+        if not user:
+            api.abort(404, f"user {user_id} does not exist")
+        db.session.delete(user)
+        db.session.commit()
+        
+        response_object['message'] = f'{email} was deleted'
+        return response_object, 200
+    
+    
+    #update CRUD 
+    #This function updates the username
+    @api.expect(user)
+    def put(self,user_id):
+        update_data = request.get_json()
+        user = User.query.filter_by(id=user_id).first()
+        email = user.email
+        response_object = {}
+        username = update_data.get('username')
+        if not user:
+            api.abort(404, f"user {user_id} does not exist")
+        if  not update_data:
+            api.abort(400, f"Input payload validation failed")
+            
+        user.username = username
+        print(user.username)
+        db.session.commit()
+        
+        response_object['message'] = f'{email} info was updated'
+        return response_object, 200
+        
+    
 class UsersList(Resource):
     
     @api.expect(user, validate=True)   
@@ -45,6 +83,11 @@ class UsersList(Resource):
     @api.marshal_with(user, as_list=True)
     def get(self):
         return User.query.all(), 200
+
+  
+    
+    
+    
     
 api.add_resource(UsersList, '/users')
 api.add_resource(Users, '/users/<int:user_id>')
--- src/tests/test_users.py
+++ src/tests/test_users.py
@@ -1,6 +1,7 @@
 import json 
 
 from src.api.models import User
+
 def test_add_user(test_app, test_database):
     client = test_app.test_client()
     resp = client.post(
@@ -14,6 +15,8 @@ def test_add_user(test_app, test_database):
     data = json.loads(resp.data.decode())
     assert resp.status_code == 201
     assert 'john@algonquincollege.com was added!' in data['message']
+    
+    
     
 def test_add_user_invalid_json(test_app, test_database): 
     client = test_app.test_client() 
@@ -65,6 +68,8 @@ def test_add_user_duplicate_email(test_app, test_database):
 
 
 
+
+
 def test_single_user(test_app, test_database,add_user):
     user = add_user('jeffrey', 'jeffrey@testdriven.io')
     client = test_app.test_client()
@@ -95,3 +100,64 @@ def test_all_users(test_app, test_database, add_user):
     assert 'john@algonquincollege.com' in data[0]['email']
     assert 'fletcher' in data[1]['username']
     assert 'fletcher@notreal.com' in data[1]['email']
+
+
+
+#test for deleting user
+def test_delete_user(test_app, test_database,add_user):
+    test_database.session.query(User).delete()
+    user1 = add_user('john', 'john@algonquincollege.com')
+    user2 = add_user('fletcher', 'fletcher@notreal.com')
+    client = test_app.test_client()
+    resp = client.delete(f'/users/{user1.id}')
+    respData = json.loads(resp.data.decode())
+    getAll = client.get('/users')
+    data = json.loads(getAll.data.decode())
+    assert resp.status_code == 200   
+    assert len(data) == 1
+    assert 'john@algonquincollege.com was deleted' in respData['message']
+    assert 'fletcher' in data[0]['username']
+    assert 'fletcher@notreal.com' in data[0]['email']
+    
+    
+    
+
+#testing for upateing username
+def test_update_user_username(test_app, test_database,add_user):
+    test_database.session.query(User).delete()
+    user1 = add_user('john', 'john@algonquincollege.com')
+    client = test_app.test_client()
+   
+    resp = client.put(
+        f'/users/{user1.id}',
+        data=json.dumps({
+            'username': 'bob',
+            }), 
+        content_type='application/json',
+    ) 
+    data = json.loads(resp.data.decode())
+    
+    getUpdatedUser = client.get(f'/users/{user1.id}')
+    updatedUserData = json.loads(getUpdatedUser.data.decode())
+    assert resp.status_code == 200
+    assert 'bob' in updatedUserData['username']
+    assert 'john@algonquincollege.com' in updatedUserData['email']
+    assert 'john@algonquincollege.com info was updated' in data['message']
+    
+
+
+      
+#testing for invalid json    
+def test_update_user_invalid_json(test_app, test_database,add_user): 
+    test_database.session.query(User).delete()
+    user1 = add_user('john', 'john@algonquincollege.com')
+    client = test_app.test_client() 
+    
+    resp = client.put(
+        f'/users/{user1.id}',
+        data=json.dumps({}), 
+        content_type='application/json',
+    ) 
+    data = json.loads(resp.data.decode()) 
+    assert resp.status_code == 400 
+    assert 'Input payload validation failed' in data['message']  
